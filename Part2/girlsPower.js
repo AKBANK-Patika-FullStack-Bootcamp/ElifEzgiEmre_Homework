f5e769809fdb1781d5d4ee9a871b7c91f7f014d7
Elif Ezgi Emre <89242843+ElifEzgiEmre@users.noreply.github.com>
2022-01-30 02:36:03 +0300

    Delete Part2 directory

--- Part2/girlsPower.js
+++
@@ -1,9 +0,0 @@
-const array = [2, 3, 4];
-
-const girlsPowerFunc= (n) => {
-    return (n/2) + 3;
-}
-
-const girlsPower = (array) => { return array.map(girlsPowerFunc);};
-
-console.log(`[${array}] ==> [${girlsPower(array)}]`);
