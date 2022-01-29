b32d59f1b54cefcabb8decd104aaa729360ea4e2
Elif Ezgi Emre <89242843+ElifEzgiEmre@users.noreply.github.com>
2022-01-30 02:36:12 +0300

    Delete Part1 directory

--- Part1/app.js
+++
@@ -1,7 +0,0 @@
-const {hex} = require('./kopek.js');
-const {Temizle, kopekBakımSaati} = require('./kopekBakımUtility.js');
-
-console.log(`Adi: ${hex.name}\t Cinsi: ${hex.breed}`);
-console.log(Temizle());
-
-console.log('köpek ile ${kopekBakımSaati} ilgilenildi.');
--- Part1/kopekBakımUtility.js
+++
@@ -1,7 +0,0 @@
-const kopekBakımSaati = 4;
-
-function Temizle(){
-    return("Köpeğiniz yıkandı.");
-}
-
-module.exports = {Temizle, kopekBakımSaati};
